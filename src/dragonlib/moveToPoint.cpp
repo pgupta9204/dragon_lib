b37264f12bcce0c74607ee47151c8a67d4209724
pgupta9204 <pranjal.nr.gupta@gmail.com>
2025-03-24 21:18:38 -0500

    done with mtp alg (what is life)

--- src/dragonlib/moveToPoint.cpp
+++ src/dragonlib/moveToPoint.cpp
@@ -16,7 +16,7 @@ void Chassis::moveToPoint(double x, double y, double cutoff, double maxSpeed, do
 
     while(timer < cutoff){
         linearError = this->position.getDistance(target);
-        angularError = this->position.getAngularError(target)-this->inertial.get_heading();
+        angularError = this->position.getDirectionTo(target)-this->inertial.get_heading();
 
         // determine whether the point is behind, in which case moving backwards is optimal
 
@@ -25,8 +25,31 @@ void Chassis::moveToPoint(double x, double y, double cutoff, double maxSpeed, do
             linearError *= -1;
         }
 
-        
+        double throttle = this->linearController.PIDUpdate(linearError, 10);
+        double turn = this->angularController.PIDUpdate(angularError, 10);
 
+        // bias towards turning
+
+        throttle *= cos(this->position.getDirectionTo(target));
+
+        // convert throttle and turn into left and right motorspots
+
+        double leftVoltage = throttle + turn;
+        double rightVoltage = throttle - turn;
+
+
+        // normalize values so we don't oversaturate and kill our motors (big nono)
+
+        if(leftVoltage > 12){
+            leftVoltage = 12;
+            rightVoltage *= 12/leftVoltage;
+        } else if (rightVoltage > 12){
+            rightVoltage = 12;
+            leftVoltage *= 12/rightVoltage;
+        }
+
+        this->leftMotorGroup.move_voltage(leftVoltage);
+        this->rightMotorGroup.move_voltage(rightVoltage);
 
         timer += 10;
         pros::delay(10);
